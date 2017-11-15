43d9e2b7371e25ee3b86482b195b77b67a57496f
Antoine FORET <antoine.foret@epitech.eu>
2017-11-15 22:12:21 +0100

    Add bundler scaffold

---
+++ lib/epitech_api.rb
@@ -0,0 +1,5 @@
+require "epitech_api/version"
+
+module EpitechApi
+  # Your code goes here...
+end
---
+++ spec/epitech_api_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe EpitechApi do
+  it "has a version number" do
+    expect(EpitechApi::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
