04f8c19e03ead7207525093d07612ae58e9bdac7
Antoine FORET <antoine.foret@epitech.eu>
2018-03-02 16:22:59 +0100

    Fixed syntax error

--- lib/epitech_api/DataTypes/student.rb
+++ lib/epitech_api/DataTypes/student.rb
@@ -3,7 +3,7 @@ require_relative 'basic_student'
 module EpitechApi
   class Student < BasicStudent
 
-    attr_accessor :login, :first_name, :last_name, :picture, :location, :closed, :time_active, :time_needed, :gpa,
+    attr_accessor :login, :first_name, :last_name, :picture, :location, :closed, :time_active, :time_needed, :gpa
 
     def initialize(infos)
       @login, @first_name, @last_name, @picture, @location, @time_active, @time_needed, @closed, @gpa =
--- lib/epitech_api/version.rb
+++ lib/epitech_api/version.rb
@@ -1,3 +1,3 @@
 module EpitechApi
-  VERSION = "0.1.3"
+  VERSION = "0.1.5"
 end
